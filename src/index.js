de6b2eaefc9339e84b796eb3b378be7f4080811c
Horbatko <142350622+Horbatko@users.noreply.github.com>
2024-10-25 00:15:41 +0300

    ///

--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
+import myPhoto from './img/photo_2024-05-29_11-01-53.png'
+
 console.log('Hello, Webpack!');
--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,67 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { type } = require('os');
 
 module.exports = {
+
   mode: 'development',
   entry: './src/index.js', 
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'img/[name][ext]',  // Куди Webpack покладе зображення
+        },
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[path][name].[ext]',
+            },
+          },
+        ],
+      },
+     
+    ],
+  },
+
   plugins: [
+
     new CleanWebpackPlugin(),
+
+
     new HtmlWebpackPlugin({
       filename: 'index.html',
       template: './src/pages/index.html'
     }),
+
     new HtmlWebpackPlugin({
       filename: 'about.html',
       template: './src/pages/about.html'
     }),
+
+    new HtmlWebpackPlugin({
+      template: './src/pages/news.html', 
+      filename: 'news.html', 
+  }),
+
+  new HtmlWebpackPlugin({
+    template: './src/pages/photo.html', 
+    filename: 'photo.html', 
+}),
+ new HtmlWebpackPlugin({
+    template: './src/pages/rozklad.html', 
+    filename: 'rozklad.html', 
+}),
   ],
+
   devServer: {
     static: {
       directory: path.join(__dirname, 'dist'), // Це замінює contentBase
